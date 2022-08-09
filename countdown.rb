352163434b483c68c5b6a18ba883885820bb23af
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-09 09:54:42 -0500

    All tests passing

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
+def countdown( seconds )
+  while(seconds > 0)
+    # s = seconds == 1 ? "" : "S"
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
 
-def countdown
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep( seconds )
+  while(seconds > 0)
+    # s = seconds == 1 ? "" : "S"
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep 1
+  end
+
+  "HAPPY NEW YEAR!"
 end
